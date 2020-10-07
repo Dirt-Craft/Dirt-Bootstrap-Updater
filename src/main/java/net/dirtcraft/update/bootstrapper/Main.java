970fba112ff891ae702f903df44d2d131254c38f
Shiny <ShinyTourettesX@gmail.com>
2020-10-07 17:39:03 +0800

    Test build

--- src/main/java/net/dirtcraft/update/bootstrapper/Main.java
+++ src/main/java/net/dirtcraft/update/bootstrapper/Main.java
@@ -1,14 +1,9 @@
 package net.dirtcraft.update.bootstrapper;
 
-import org.apache.commons.io.FileUtils;
-
 import java.io.File;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class Main {
@@ -16,16 +11,14 @@ public class Main {
         System.out.println("Update bootstrap initializing...");
         String jvm = args[0];
         File jar = new File(args[1]);
-        File temp = new File(args[1] + ".temp");
-        URL url = new URL(args[2]);
+        File temp = new File(args[2]);
         args = removeFirstEntries(args, 3);
 
-        copyUrlToFile(url, temp);
-        replaceSingleFile(temp, jar);
+        replaceSingleFile(jar, temp);
         launch(jvm, jar, args);
     }
 
-    public static void replaceSingleFile(File src, File dest){
+    public static void replaceSingleFile(File dest, File src){
         System.out.println("Copying data...");
         dest.delete();
         src.renameTo(dest);
@@ -39,13 +32,6 @@ public class Main {
         new ProcessBuilder(args)
                 .inheritIO()
                 .start();
-    }
-
-    private static void copyUrlToFile(URL url, File file) throws Throwable{
-        System.out.println("Downloading new update, Please wait...");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-        FileUtils.copyInputStreamToFile(con.getInputStream(), file);
     }
 
     private static String[] removeFirstEntries(String[] args, int i){
